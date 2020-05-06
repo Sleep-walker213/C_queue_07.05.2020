2306337484f4b9eaa3496b50a28dd56f3d820b12
Sleep-walker213 <64605307+Sleep-walker213@users.noreply.github.com>
2020-05-06 22:09:54 +0300

    Add files via upload

--- queue.h
+++ queue.h
@@ -1,5 +1,13 @@
-#ifndef __queue__h
-#define __queue__h
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+struct node{
+    int elem;
+    struct node * next;
+};
+
+struct queue{
+    struct node *f, *l;
+};
 void create (struct queue **q);
 bool empty(struct queue *q);
 void push (struct queue**q, int x);
