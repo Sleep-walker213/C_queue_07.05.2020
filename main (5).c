243c7fd556e7bb9e89569fd7ba5d548c4d36d5f0
Sleep-walker213 <64605307+Sleep-walker213@users.noreply.github.com>
2020-05-06 21:14:20 +0300

    Add files via upload

---
+++ main (5).c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct node{
+    int elem;
+    struct node * next;
+};
+
+struct queue{
+    struct node *f, *l;
+};
+
+void create (struct queue **q)
+{
+    (*q)=malloc(sizeof(struct queue));
+    (*q)->f=NULL;
+    (*q)->l=NULL;
+}
+
+bool empty(struct queue *q)
+{
+    return (q->f==NULL);
+}
+
+void push (struct queue**q, int x)
+{
+    struct node *p;
+    p=malloc(sizeof(struct node));
+    p->elem=x;
+    p->next=NULL;
+    if (empty(*q)) (*q)->f=p;
+    else
+    {
+        (*q)->l->next=p;
+    }
+    (*q)->l=p;
+}
+
+int pop (struct queue**q)
+{
+    if (empty(*q)) exit(1);
+    struct node *p;
+    p=(*q)->f;
+    int x;
+    x=p->elem;
+    (*q)->f=p->next;
+    free(p);
+    if ((*q)->f==NULL) ((*q)->l=NULL);
+    return x;
+}
+
+void print (struct queue *q)
+{
+    struct node *p;
+    p=q->f;
+    while (p!=NULL)
+    {
+        printf("%d ",p->elem);
+        p=p->next;
+    }
+    putchar('\n');
+}
+
+int main()
+{
+    int x;
+    struct queue *q;
+    create(&q);
+    while ((scanf("%d", &x)!=0) && (x!='.'))
+    push(&q, x);
+    pop(&q);
+    print(q);
+    push(&q, 10);
+    print(q);
+}
